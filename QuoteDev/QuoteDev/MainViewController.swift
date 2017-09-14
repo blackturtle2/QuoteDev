56c8b35eb812d01cf607a41648bb00b23f900984
leejaesung <blackturtle2@gmail.com>
2017-09-14 14:49:55 +0900

    Create - 새 프로젝트 시작
    새 프로젝트를 시작하였습니다.

---
+++ QuoteDev/QuoteDev/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuoteDev
+//
+//  Created by leejaesung on 2017. 9. 14..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
