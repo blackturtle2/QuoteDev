7db866c8e40fd9bbe452359b4fd0cc49566c49a4
leejaesung <blackturtle2@gmail.com>
2017-09-19 01:20:10 +0900

    [Create] 명언 댓글 버튼을 눌렀을 때, 닉네임 설정 Alert 뜨도록 작업중
    UiAlertController에 `textField` 추가하였고, completion을 정의할 차례입니다.
    여기저기 쓰일 부분이어서 신중하게 만들어야겠네요.

--- QuoteDev/QuoteDev/MainViewController.swift
+++ QuoteDev/QuoteDev/MainViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet var buttonLike : UIButton! //명언 좋아요 버튼
+    @IBOutlet var buttonComment : UIButton! //명언 댓글 버튼
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,29 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // MARK: 명언 좋아요 버튼 액션
+    @IBAction func buttonLikeAction(_ sender: UIButton) {
+        
+    }
 
+    // MARK: 명언 댓글 버튼 액션
+    @IBAction func buttonCommentAction(_ sender: UIButton) {
+        
+        let alertSetUserNickname:UIAlertController = UIAlertController(title: "닉네임 설정", message: "닉네임을 설정해주세요.", preferredStyle: .alert)
+        
+        alertSetUserNickname.addTextField { (textField) in
+            textField.placeholder = "스티브 잡스"
+        }
+        
+        alertSetUserNickname.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alertSetUserNickname] (_) in
+            let textField = alertSetUserNickname!.textFields![0] // 위에서 추가한 텍스트필드이므로 옵셔널 바인딩은 스킵.
+            print("///// textField: ", textField.text ?? "(no data)")
+        }))
+        
+        self.present(alertSetUserNickname, animated: true, completion: nil)
+    }
 
 }
 
