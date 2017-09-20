a1f6641fe79633a505dfadd0350df1290d08d981
leejaesung <blackturtle2@gmail.com>
2017-09-20 19:15:23 +0900

    [Update] Firebase Auth 가입 및 Database 닉네임 저장 기능 구현
    파이어베이스의 익명 Auth와 Database에 닉네임 저장 기능까지 구현하였습니다. ( `UserDefaults`에 동시 저장 )
    
    메인 뷰에서 댓글 버튼을 누르면, `UserDefaults`에 닉네임이 있는지 조회하고, 없으면 닉네임을 입력하는 Alert가 표시됩니다.
    
    // TODO
    1. 텍스트필드가 비어 있는 케이스의 예외처리.
    2. DB에 중복되는 닉네임이 있을 케이스의 예외처리. OTL

---
+++ QuoteDev/QuoteDev/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  QuoteDev
+//
+//  Created by leejaesung on 2017. 9. 20..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let userDefaults_Uid:String = "firebaseUserUid"       // UserDefaults에 저장하는 Uid의 UserDefaults Key값입니다.
+    static let userDefaults_UserNickname:String = "userNickname" //
+    
+    static let quoteCommentViewController:String = "QuoteCommentViewController"
+    
+    static let firebaseUsersRoot:String = "Users"
+    static let firebaseUserUid:String = "userUid"
+    static let firebaseUserNickname:String = "userNickname"
+}
+
--- QuoteDev/QuoteDev/DataCenter.swift
+++ QuoteDev/QuoteDev/DataCenter.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-// UserDefaults에 저장하는 Uid의 UserDefaults Key값입니다.
-let userDefaultsUid:String = "firebaseUserUid"
-
 
 // 추후 사용할 DataCenter 입니다.
 class DataCenter {
