c16168cfd2bdb2f2536b961a25fc2d669e12c036
GisuHwang <kisu9838@gmail.com>
2017-09-21 15:53:33 +0900

    [주의] Merge branch 'dev' into gisu
    스토리보드 충돌나서 기존제가 한부분 Discard 하고 커밋합니다.

--- QuoteDev/QuoteDev/MainViewController.swift
+++ QuoteDev/QuoteDev/MainViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import Firebase
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet var buttonLike : UIButton! //명언 좋아요 버튼
+    @IBOutlet var buttonComment : UIButton! //명언 댓글 버튼
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // MARK: 명언 좋아요 버튼 액션
+    @IBAction func buttonLikeAction(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: 명언 댓글 버튼 액션
+    @IBAction func buttonCommentAction(_ sender: UIButton) {
+        
+        // UserDefaults에 사용자 닉네임이 없으면, 닉네임을 받습니다.
+        if UserDefaults.standard.string(forKey: Constants.userDefaults_UserNickname) == nil {
+            
+            // UIAlertController 생성
+            let alertSetUserNickname:UIAlertController = UIAlertController(title: "닉네임 설정", message: "닉네임을 설정해주세요.", preferredStyle: .alert)
+            
+            // testField 추가
+            alertSetUserNickname.addTextField { (textField) in
+                textField.placeholder = "스티브 잡스"
+            }
+            
+            // OK 버튼 Action 추가
+            alertSetUserNickname.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alertSetUserNickname] (_) in
+                
+                // 텍스트필드 호출
+                let textFieldNickname = alertSetUserNickname!.textFields![0] // 위에서 직접 추가한 텍스트필드이므로 옵셔널 바인딩은 스킵.
+                print("///// textField: ", textFieldNickname.text ?? "(no data)")
+                
+                // UserDefaults 에서 uid 호출 & 사용자가 텍스트필드에 입력한 텍스트 호출
+                guard let uid = UserDefaults.standard.string(forKey: Constants.userDefaults_Uid) else { return }
+                guard let userNickname = alertSetUserNickname!.textFields?[0].text else { return }
+                
+                let dicUserData:[String:Any] = [Constants.firebaseUserUid:uid, Constants.firebaseUserNickname:userNickname]
+                
+                // Firebase DB & UserDefaults에 저장
+                Database.database().reference().child(Constants.firebaseUsersRoot).child(uid).setValue(dicUserData)
+                UserDefaults.standard.set(userNickname, forKey: Constants.userDefaults_UserNickname)
+                
+                // 명언 댓글 뷰로 이동
+                // 닉네임이 있을 경우, 스토리보드 상에 선언된 show를 타서 이동합니다.
+                let nextVC = self.storyboard?.instantiateViewController(withIdentifier: Constants.quoteCommentViewController) as! QuoteCommentViewController
+                self.navigationController?.pushViewController(nextVC, animated: true)
+                
+            }))
+            
+            self.present(alertSetUserNickname, animated: true, completion: nil)
+            
+        }
+        
+        
+        
+    }
+    
 }
 
---
+++ QuoteDev/QuoteDev/QuoteCommentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuoteCommentViewController.swift
+//  QuoteDev
+//
+//  Created by leejaesung on 2017. 9. 20..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class QuoteCommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
