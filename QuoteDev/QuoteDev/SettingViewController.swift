6cba14eaa82808262157f2969d34a301f4be0ee3
leejaesung <blackturtle2@gmail.com>
2017-10-09 18:46:53 +0900

    [Create] 개발자 문의 메일 보내기 기능 구현 완료
    설정 뷰에서 개발자 문의 기능을 구현 완료하였습니다.

--- QuoteDev/QuoteDev/SettingViewController.swift
+++ QuoteDev/QuoteDev/SettingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class SettingViewController: UIViewController {
     
@@ -31,6 +32,32 @@ class SettingViewController: UIViewController {
     //MARK:-         Functions                 //
     /*******************************************/
     
+    //MARK: 개발자 문의 email 보내기 function 정의
+    // [주의] `MessageUI` import 필요
+    func sendEmailTo(emailAddress email:String) {
+        let userSystemVersion = UIDevice.current.systemVersion // 현재 사용자 iOS 버전
+        let userAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String // 현재 사용자 앱 버전
+        
+        // 메일 쓰는 뷰컨트롤러 선언
+        let mailComposeViewController = configuredMailComposeViewController(emailAddress: email, systemVersion: userSystemVersion, appVersion: userAppVersion!)
+        
+        //사용자의 아이폰에 메일 주소가 세팅되어 있을 경우에만 mailComposeViewController()를 태웁니다.
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } // else일 경우, iOS 에서 자체적으로 메일 주소를 세팅하라는 메시지를 띄웁니다.
+    }
+    
+    // MARK: 메일 보내는 뷰컨트롤러 속성 세팅
+    func configuredMailComposeViewController(emailAddress:String, systemVersion:String, appVersion:String) -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // 메일 보내기 Finish 이후의 액션 정의를 위한 Delegate 초기화.
+        
+        mailComposerVC.setToRecipients([emailAddress]) // 받는 사람 설정
+        mailComposerVC.setSubject("[QuoteDev] 사용자로부터 도착한 편지") // 메일 제목 설정
+        mailComposerVC.setMessageBody("* iOS Version: \(systemVersion) / App Version: \(appVersion)\n** 고맙습니다. 무엇이 궁금하신가요? :D", isHTML: false) // 메일 내용 설정
+        
+        return mailComposerVC
+    }
     
 }
 
@@ -143,12 +170,36 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
         
     }
     
-    // MARK: tableView - DidSelectRowAt
+    // MARK: tableView - cell 선택하기
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // 터치한 표시를 제거하는 액션
         tableView.deselectRow(at: indexPath, animated: true)
         
+        switch indexPath.section {
+        case enumSettingSection.about.rawValue:
+            switch indexPath.row{
+            case 0: //개발자 소개
+                return
+            case 1: //개발자 문의
+                // 개발자에게 메일을 보냅니다.
+                self.sendEmailTo(emailAddress: "blackturtle2@gmail.com")
+            case 2: //앱 버전
+                return
+            default:
+                return
+            }
+        default:
+            return
+        }
+        
     }
     
+}
+
+// MARK: Extension - MFMailComposeViewControllerDelegate
+extension SettingViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
