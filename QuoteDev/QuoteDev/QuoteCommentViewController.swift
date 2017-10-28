cd0814406881826fb2ec8c8d5f93c88c0ab4f5ba
leejaesung <blackturtle2@gmail.com>
2017-10-28 11:49:45 +0900

    [Update] 명언 댓글 뷰 초안 디자인 구현
    댓글 작성 뷰 박스까지 구현하였습니다.

--- QuoteDev/QuoteDev/QuoteCommentViewController.swift
+++ QuoteDev/QuoteDev/QuoteCommentViewController.swift
@@ -10,26 +10,46 @@ import UIKit
 
 class QuoteCommentViewController: UIViewController {
 
+    @IBOutlet weak var tableViewMain: UITableView!
+    
+    @IBOutlet weak var viewWritingCommentBox: UIView!
+    @IBOutlet weak var textFieldWritingComment: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableViewMain.delegate = self
+        self.tableViewMain.dataSource = self
+        
+        self.textFieldWritingComment.layer.borderColor = UIColor.black.cgColor
+        self.textFieldWritingComment.layer.borderWidth = 1.0
+//        self.textFieldWritingComment.borderStyle = .roundedRect
+        self.textFieldWritingComment.layer.cornerRadius = 5; // borderStyle이 먹지 않는 관계로.. cornerRadius를 강제로 삽입합니다.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+/*******************************************/
+//MARK:-         extenstion                //
+/*******************************************/
+extension QuoteCommentViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "comment_cell", for: indexPath)
+        
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
 }
